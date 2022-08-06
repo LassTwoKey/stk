f4cb31f87c6ed83a87107f254077c14ac2ca6cef
LassTwoKey <83500227+LassTwoKey@users.noreply.github.com>
2022-08-07 00:57:28 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5360,6 +5360,13 @@
             delay: 300
         });
     }));
+    window.onload = function() {
+        document.body.classList.add("loaded_hiding");
+        window.setTimeout((function() {
+            document.body.classList.add("loaded");
+            document.body.classList.remove("loaded_hiding");
+        }), 1e3);
+    };
     window["FLS"] = false;
     isWebp();
     menuInit();
